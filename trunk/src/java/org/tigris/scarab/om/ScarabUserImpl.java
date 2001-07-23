ca260a1fd94e10b6d21c4924142e03d9fcde369e
dlr <dlr@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2001-07-23 23:11:35 +0000

    Add constant denoting the maximum length for the unique identifier used at user creation time.
    git-svn-id: http://scarab.tigris.org/svn/scarab@734 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/ScarabUserImpl.java
+++ trunk/src/java/org/tigris/scarab/om/ScarabUserImpl.java
@@ -80,6 +80,12 @@ public class ScarabUserImpl extends BaseScarabUserImpl implements ScarabUser
     private static final String REPORTING_ISSUE_START_POINT = "RISP";
 
     /**
+        The maximum length for the unique identifier used at user
+        creation time.
+    */
+    private static final int UNIQUE_ID_MAX_LEN = 10;
+
+    /**
         Call the superclass constructor to initialize this object.
     */
     public ScarabUserImpl()
@@ -140,9 +146,9 @@ public class ScarabUserImpl extends BaseScarabUserImpl implements ScarabUser
     {
         // get a unique id for validating the user
         String uniqueId = GenerateUniqueId.getIdentifier();
-        if (uniqueId.length() > 10)
+        if (uniqueId.length() > UNIQUE_ID_MAX_LEN)
         {
-            uniqueId = uniqueId.substring(0,10);
+            uniqueId = uniqueId.substring(0, UNIQUE_ID_MAX_LEN);
         }
         // add it to the perm table
         setConfirmed(uniqueId);
