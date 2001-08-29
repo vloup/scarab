7d3dcf4c010beb06f9811b02b0435fab641c8ec5
dlr <dlr@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2001-08-29 23:35:49 +0000

    Added correct get/setUser doc.
    git-svn-id: http://scarab.tigris.org/svn/scarab@1104 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/tools/ScarabRequestScope.java
+++ trunk/src/java/org/tigris/scarab/tools/ScarabRequestScope.java
@@ -84,20 +84,24 @@ public interface ScarabRequestScope extends ApplicationTool
     public Attribute getAttribute() throws Exception;
 
     /**
-     * A User object for use within the Scarab API. This
-     * is the result of whatever was set with setUser().
-     * It can return null if setUser() has not been previously
-     * called. If you would like to get the currently logged
-     * in User, retrieve that from the data.getUser() method.
+     * A <code>User</code> object for use within the Scarab API,
+     * generally <i>not</i> the user who is logged in.
+     *
+     * @param user A user used during this request.
      */
     public void setUser (ScarabUser user);
 
     /**
-     * A User object for use within the Scarab API. This
-     * is the result of whatever was set with setUser().
-     * It can return null if setUser() has not been previously
-     * called. If you would like to get the currently logged
-     * in User, retrieve that from the data.getUser() method.
+     * A <code>User</code> object for use within the Scarab API. This
+     * is the result of whatever was set with <code>setUser()</code>
+     * (generally <i>not</i> the user who is logged in).  It can
+     * return <code>null</code> if <code>setUser()</code> has not been
+     * previously called.  If you would like to get the currently
+     * logged in <code>User</code>, retrieve that from the
+     * data.getUser() method.
+     *
+     * @return A user used during this request.
+     * @see org.tigris.scarab.tools.ScarabRequestTool#setUser(ScarabUser)
      */
     public ScarabUser getUser();
 
