fddcc2f4a0b6f7507110fd2e337b8bd200077111
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2001-08-31 18:10:58 +0000

    removed tabs
    git-svn-id: http://scarab.tigris.org/svn/scarab@1175 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/test/org/tigris/scarab/om/IssueTest.java
+++ trunk/src/test/org/tigris/scarab/om/IssueTest.java
@@ -80,8 +80,8 @@ public class IssueTest extends BaseTestCase
         issue.setModuleCast(getModule());
 
         testGetAllAttributeValuesMap(issue);
-	testGetUniqueId(issue);
-	testGetEligibleAssignees(issue); 
+        testGetUniqueId(issue);
+        testGetEligibleAssignees(issue); 
     }
     
     private void testGetAllAttributeValuesMap(Issue issue) throws Exception
@@ -89,27 +89,25 @@ public class IssueTest extends BaseTestCase
         System.out.println ("testGetAllAttributeValuesMap()");
         HashMap map = issue.getAllAttributeValuesMap();
         System.out.println ("getAllAttributeValuesMap().size(): " + map.size());
-        assertEquals (map.size(), 10);	
+        assertEquals (map.size(), 10);  
     }
 
     private void testGetUniqueId(Issue issue) throws Exception
     {
         System.out.println ("testGetUniqueId()");
 
-	String strUniqueID = null;
-	strUniqueID = issue.getUniqueId();
-	System.out.println ("Unique id: " + strUniqueID);	
+        String strUniqueID = null;
+        strUniqueID = issue.getUniqueId();
+        System.out.println ("Unique id: " + strUniqueID);   
     }
 
     private void testGetEligibleAssignees(Issue issue) throws Exception
     {
         System.out.println ("testGetEligibleAssignees()");
 
-	java.util.List eligibleAssignees = null;
-	eligibleAssignees = issue.getEligibleAssignees();
-	System.out.println ("Number of eligible assignees: "
-		+ eligibleAssignees.size());
-	
+        java.util.List eligibleAssignees = null;
+        eligibleAssignees = issue.getEligibleAssignees();
+        System.out.println ("Number of eligible assignees: "
+            + eligibleAssignees.size());
     }
-
 }
