0b60f5cbfe6e85314bb6af23855317d6b750b675
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2001-10-19 23:59:11 +0000

    commented out debugging Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://scarab.tigris.org/svn/scarab@1822 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/admin/ModifyModuleAttributes.java
+++ trunk/src/java/org/tigris/scarab/actions/admin/ModifyModuleAttributes.java
@@ -330,9 +330,9 @@ public class ModifyModuleAttributes extends RequireLoginFirstAction
         ScarabModule module = (ScarabModule)scarabR.getCurrentModule();
         IssueType issueType = scarabR.getIssueType();
         RModuleIssueType rmit = module.getRModuleIssueType(issueType);
-System.out.println(rmit);
-System.out.println(rmit.getIssueTypeId());
-System.out.println(rmit.getDisplayName());
+//System.out.println(rmit);
+//System.out.println(rmit.getIssueTypeId());
+//System.out.println(rmit.getDisplayName());
         List attGroups = issueType.getAttributeGroups(module);
         List attributeGroups = issueType.getAttributeGroups(module);
 
@@ -392,7 +392,7 @@ System.out.println(rmit.getDisplayName());
             // Set properties for module-issue type info
             Group rmitGroup = intake.get("RModuleIssueType", 
                                         rmit.getQueryKey(), false);
-System.out.println(rmit);
+//System.out.println(rmit);
             rmitGroup.setProperties(rmit);
             rmit.save();
            
