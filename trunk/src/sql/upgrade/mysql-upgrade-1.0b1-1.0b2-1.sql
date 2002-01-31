99742d8443004f96bacab9e37a148b292f86f8b5
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-01-31 01:08:24 +0000

    fixed case of new column
    added comments
    
    cleanup
    
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@3201 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/sql/upgrade/mysql-upgrade-1.0b1-1.0b2-1.sql
+++ trunk/src/sql/upgrade/mysql-upgrade-1.0b1-1.0b2-1.sql
@@ -1,4 +1,13 @@
-ALTER TABLE SCARAB_R_MODULE_ISSUE_TYPE add column Dedupe int(1) not null default 1;     
-INSERT INTO SCARAB_R_MODULE_ISSUE_TYPE (dedupe) SELECT Dedupe from SCARAB_MODULE
-;       
+/*
+ * This upgrade script moves the DEDUPE column and its contents from
+ * the SCARAB_MODULE table to the SCARAB_R_MODULE_ISSUE_TYPE table.
+ *
+ * Created By: Elicia David
+ * $Id$
+ */
+
+ALTER TABLE SCARAB_R_MODULE_ISSUE_TYPE add column DEDUPE int(1) not null default 1;
+
+INSERT INTO SCARAB_R_MODULE_ISSUE_TYPE (dedupe) SELECT DEDUPE from SCARAB_MODULE;
+
 ALTER TABLE SCARAB_MODULE drop DEDUPE;
