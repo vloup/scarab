818561355459d12201777d7f62ad6b46f0ef2436
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-02-04 22:24:16 +0000

    inactive issue types are currently appearing on the select issue type screen. users can select an inactive issue type and enter issues against that issue type. - 7924
    this change is untested since my sandbox is currently broken,
    but i think that it should fix the issue.
    
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@3265 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/services/module/AbstractScarabModule.java
+++ trunk/src/java/org/tigris/scarab/services/module/AbstractScarabModule.java
@@ -617,7 +617,8 @@ public abstract class AbstractScarabModule
     }
 
     /**
-     * gets a list of the Issue Types for this module.
+     * gets a list of the Issue Types for this module. only shows
+     * active issue types
      */
     public List getIssueTypes()
         throws Exception
@@ -625,7 +626,8 @@ public abstract class AbstractScarabModule
         Criteria crit = new Criteria();
         crit.addJoin(RModuleIssueTypePeer.ISSUE_TYPE_ID, 
                      IssueTypePeer. ISSUE_TYPE_ID);
-        crit.add(RModuleIssueTypePeer. MODULE_ID, getModuleId());
+        crit.add(RModuleIssueTypePeer.MODULE_ID, getModuleId());
+        crit.add(RModuleIssueTypePeer.ACTIVE, 1);
         crit.add(IssueTypePeer.PARENT_ID, 0);
         crit.add(IssueTypePeer.DELETED, 0);
         return IssueTypePeer.doSelect(crit);
