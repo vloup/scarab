550fbf34ee8696d144d6a648c70111f2da99d199
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-02-13 19:59:20 +0000

    moved attributegroup method here, from IssueType - more consistant.
    Also changed getRequiredAttributes() to check that the attribute's group is active before marking it required. Now returns a List.
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@3467 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/services/module/ModuleEntity.java
+++ trunk/src/java/org/tigris/scarab/services/module/ModuleEntity.java
@@ -170,8 +170,48 @@ public interface ModuleEntity
 
     public void save() throws Exception;
 
-    public List getRModuleAttributes(IssueType issueType, boolean activeOnly,
-                                     String attributeType)
+    /**
+     * Creates new attribute group.
+     */
+    public AttributeGroup createNewGroup (IssueType issueType)
+        throws Exception;
+
+    /**
+     * Returns the attribute group this attribute is associated with.
+     */
+    public AttributeGroup getAttributeGroup(IssueType issueType, 
+                                            Attribute attribute)
+        throws Exception;
+
+    /**
+     * List of active attribute groups associated with this module.
+     */
+    public List getAttributeGroups(IssueType issueType)
+        throws Exception;
+
+    /**
+     * List of attribute groups associated with this module).
+     */
+    public List getAttributeGroups(IssueType issueType, boolean activeOnly)
+        throws Exception;
+
+    /**
+     * List of active dedupe attribute groups associated with this module.
+     */
+    public List getDedupeAttributeGroups(IssueType issueType)
+        throws Exception;
+
+    /**
+     * List of attribute groups associated with this module.
+     */
+    public List getDedupeAttributeGroups(IssueType issueType,
+                                         boolean activeOnly)
+        throws Exception;
+
+    /**
+     * Gets the sequence where the dedupe screen fits between groups.
+     */
+    public int getDedupeSequence(IssueType issueType)
         throws Exception;
 
     public List getRModuleAttributes(IssueType issueType, boolean activeOnly)
@@ -300,7 +340,7 @@ public interface ModuleEntity
      * @param inOrder flag determines whether the attribute order is important
      * @return an <code>Attribute[]</code> value
      */
-    public Attribute[] getRequiredAttributes(IssueType issueType)
+    public List getRequiredAttributes(IssueType issueType)
         throws Exception;
 
     /**
