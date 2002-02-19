0e5308f59d2e93573f2c5c0935cfd4f50ca7c535
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-02-19 19:32:01 +0000

    add javadoc
    git-svn-id: http://scarab.tigris.org/svn/scarab@3576 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/tools/ScarabGlobalTool.java
+++ trunk/src/java/org/tigris/scarab/tools/ScarabGlobalTool.java
@@ -377,11 +377,24 @@ public class ScarabGlobalTool implements ScarabGlobalScope
         return b;
     }
 
+    /**
+     * Logs a message at the debug level.  Useful for "I am here" type 
+     * messages. The category is "org.tigris.scarab". 
+     *
+     * @param s message to log
+     */
     public void log(String s)
     {
         log.debug(s);
     }
 
+    /**
+     * Logs a message at the debug level.  Useful for "I am here" type 
+     * messages. The category in which to log is also specified. 
+     *
+     * @param category log4j Category
+     * @param s message to log
+     */
     public void log(String category, String s)
     {
         Category.getInstance(category).debug(s);
