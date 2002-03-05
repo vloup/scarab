67b7bdf3752496a5283c2267d13f10baeac24287
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-03-05 02:18:11 +0000

    added logic to determine which type of attributes should be saved, data or user
    git-svn-id: http://scarab.tigris.org/svn/scarab@3755 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/admin/GlobalAttributes.java
+++ trunk/src/java/org/tigris/scarab/actions/admin/GlobalAttributes.java
@@ -81,16 +81,20 @@ public class GlobalAttributes extends RequireLoginFirstAction
     public void doSave( RunData data, TemplateContext context ) 
         throws Exception
     {
-        String template = data.getParameters()
-            .getString(ScarabConstants.TEMPLATE, null);
-        String nextTemplate = data.getParameters().getString(
-            ScarabConstants.NEXT_TEMPLATE, template );
-
         IntakeTool intake = (IntakeTool)context
             .get(ScarabConstants.INTAKE_TOOL);
+        String attributeType = data.getParameters().getString("attributeType");
         if ( intake.isAllValid() ) 
         {
-            List allAttributes = AttributePeer.getAttributes();
+            List allAttributes;
+            if (attributeType != null && attributeType.equals("user"))
+            {
+                allAttributes = AttributePeer.getAttributes("user");
+            }
+            else
+            {
+                allAttributes = AttributePeer.getAttributes();
+            }
             for (int i=0;i<allAttributes.size();i++)
             {
                 Attribute attr = (Attribute) allAttributes.get(i);
@@ -119,10 +123,8 @@ public class GlobalAttributes extends RequireLoginFirstAction
     public void doCreatenew( RunData data, TemplateContext context )
         throws Exception
     {
-        String template = data.getParameters()
-            .getString(ScarabConstants.TEMPLATE, null);
         String nextTemplate = data.getParameters().getString(
-            ScarabConstants.OTHER_TEMPLATE, template );
+            ScarabConstants.OTHER_TEMPLATE, "admin, GlobalAttributeEdit.vm" );
         setTarget(data, nextTemplate);
 
         ScarabRequestTool scarabR = getScarabRequestTool(context);
