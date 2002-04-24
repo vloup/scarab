cba41f673a5422ebdc9ecc73ebfbcfe08b55870b
(no author) <(no author)@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-04-24 20:25:55 +0000

    This commit was manufactured by cvs2svn to create tag 'TIGRIS_1_3_0_TR20'.
    git-svn-id: http://scarab.tigris.org/svn/scarab@4279 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/TIGRIS_1_3_0_TR20/src/java/org/tigris/scarab/om/ScarabModuleContainer.java
@@ -0,0 +1,43 @@
+package org.tigris.scarab.om;
+
+import java.util.*;
+import org.apache.torque.om.BaseObject;
+import org.apache.torque.util.BasePeer;
+import org.apache.torque.util.Criteria;
+import org.apache.torque.pool.DBConnection;
+
+
+/** 
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @author <a href="mailto:dr@bitonic.com">Douglas B. Robertson</a>
+ * @author <a href="mailto:jmcnally@collab.new">John McNally</a>
+ * @version $Id$
+ */
+public class ScarabModuleContainer extends org.tigris.scarab.om.ScarabModule
+{
+    public ScarabModuleContainer()
+    {
+        setClassKey(ScarabModulePeer.CLASSKEY_1);
+    }
+        
+    /**
+     * Determines whether this module is accepting new issues.
+     * Containers never accept new issues, so this will return false;
+     */
+    public boolean allowsNewIssues() 
+    {
+        return false;
+    }
+    
+    /**
+     * Determines whether this module accepts issues.  This default
+     * implementation does NOT allow issues.
+     */
+    public boolean allowsIssues()
+    {
+        return false;
+    }
+}
