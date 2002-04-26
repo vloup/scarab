52d44371915d0b7d1ca290c7a22e4fe56e8a963a
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-04-26 21:12:31 +0000

    removed use of deprecated BaseObject.getCategory() and replaced it with BaseObject.log().
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@4322 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
+++ trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
@@ -1749,7 +1749,7 @@ try{
         // from parent module
         NumberKey newModuleId = getModuleId();
         Module parentModule = ModuleManager.getInstance(getParentId());
-        getCategory().debug("[ASM] parent name=" + parentModule.getRealName());
+        log().debug("[ASM] parent name=" + parentModule.getRealName());
         AttributeGroup ag1;
         AttributeGroup ag2;
         RModuleAttribute rma1 = null;
@@ -1763,7 +1763,7 @@ try{
                 (RModuleIssueType)templateTypes.get(i);
             RModuleIssueType template2 = template1.copy();
             template2.setModuleId(newModuleId);
-            getCategory().debug("[ASM] Saving new template type: " + template2.getModuleId()
+            log().debug("[ASM] Saving new template type: " + template2.getModuleId()
                                 + "-" + template2.getIssueTypeId());
             template2.save();
             
@@ -1777,7 +1777,7 @@ try{
                 rma2.setModuleId(newModuleId);
                 rma2.setAttributeId(rma1.getAttributeId());
                 rma2.setIssueTypeId(rma1.getIssueTypeId());
-                getCategory().debug("[ASM] Saving rma for new template type: " + 
+                log().debug("[ASM] Saving rma for new template type: " + 
                                     rma2.getModuleId()
                                     + "-" + rma2.getIssueTypeId() + "-" +
                                     rma2.getAttributeId());
