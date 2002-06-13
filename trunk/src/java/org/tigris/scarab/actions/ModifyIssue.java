12343076015f6ab8d20fdba6f15f41ea1f805272
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-06-13 22:05:41 +0000

    use the new attachment_id column in activity
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@4835 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/ModifyIssue.java
+++ trunk/src/java/org/tigris/scarab/actions/ModifyIssue.java
@@ -715,14 +715,14 @@ public class ModifyIssue extends BaseModifyIssue
         // Save transaction record
         Transaction transaction = new Transaction();
         transaction
-            .create(TransactionTypePeer.EDIT_ISSUE__PK, user, attachment);
+            .create(TransactionTypePeer.EDIT_ISSUE__PK, user);
 
         // Save activity record
         Activity activity = new Activity();
-        activity.create(issue, null, description, transaction, oldVal, newVal);
+        activity.create(issue, null, description, transaction, 
+                        oldVal, newVal, attachment);
         sendEmail(transaction, issue, message, context, data);
     }
-
 
     /**
     *  Modifies the dependency type between the current issue
