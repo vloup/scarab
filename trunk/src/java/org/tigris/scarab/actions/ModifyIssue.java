6c5cbddc5fba476c60e86bd30308713492abefa1
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-07-15 19:22:34 +0000

    do not show the Edit Assignee's button if the user does not have permission and don't redirect in the action in case the user attempts it.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@5152 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/ModifyIssue.java
+++ trunk/src/java/org/tigris/scarab/actions/ModifyIssue.java
@@ -94,6 +94,7 @@ import org.tigris.scarab.om.DependTypePeer;
 import org.tigris.scarab.om.ScarabUser;
 import org.tigris.scarab.tools.ScarabRequestTool;
 import org.tigris.scarab.services.cache.ScarabCache; 
+import org.tigris.scarab.services.security.ScarabSecurity;
 import org.apache.fulcrum.TurbineServices;
 import org.apache.fulcrum.upload.TurbineUploadService;
 import org.apache.fulcrum.upload.UploadService;
@@ -970,9 +971,18 @@ public class ModifyIssue extends BaseModifyIssue
             scarabR.setAlertMessage("Could not locate issue: " + id);
             return;
         }
-        data.getParameters().add("issue_ids", 
-              issue.getUniqueId());
-        setTarget(data, "AssignIssue.vm");            
+        ScarabUser user = (ScarabUser)data.getUser();
+        if (user.hasPermission(ScarabSecurity.ISSUE__ASSIGN, 
+                               issue.getModule()))
+        {
+            data.getParameters().add("issue_ids", issue.getUniqueId());
+            setTarget(data, "AssignIssue.vm");
+        }
+        else
+        {
+            scarabR.setAlertMessage(
+                "Insufficient permissions to assign users.");
+        }
     }
 
     /**
