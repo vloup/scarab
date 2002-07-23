7a1174d966c0269d7102dd7fca9afbcacf83d436
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-07-23 21:58:43 +0000

    added patch from janson@vsnl.com
    This exception is coming when we try to "Add/remove attributes from view". I think this is coming because we're not setting
    the userid while trying to save the view. I've a small patch for this. I don't know whether this is correct or not
    
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@5234 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/AbstractScarabUser.java
+++ trunk/src/java/org/tigris/scarab/om/AbstractScarabUser.java
@@ -1216,6 +1216,7 @@ public abstract class AbstractScarabUser
                 rmua = getNewRModuleUserAttribute(attribute, module, issueType);
             }
             rmua.setOrder(i++);
+            rmua.setUserId(getUserId());
             rmua.save();
         }
     }
