df1c44e934d14001de1126266fe1c65ce985b2de
(no author) <(no author)@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-07-31 02:11:48 +0000

    This commit was manufactured by cvs2svn to create tag 'TIGRIS_2_0_1_TR13'.
    git-svn-id: http://scarab.tigris.org/svn/scarab@5300 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/TIGRIS_2_0_1_TR13/src/java/org/tigris/scarab/actions/base/ScarabTemplateAction.java
@@ -0,0 +1,254 @@
+package org.tigris.scarab.actions.base;
+
+/* ================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */
+ 
+// Java Stuff
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Iterator;
+
+ // Turbine Stuff
+import org.apache.log4j.Category;
+import org.apache.turbine.RunData;
+import org.apache.turbine.TemplateAction;
+import org.apache.turbine.TemplateContext;
+import org.apache.turbine.tool.IntakeTool;
+import org.apache.turbine.ParameterParser;
+
+// Scarab Stuff
+import org.tigris.scarab.util.ScarabConstants;
+import org.tigris.scarab.tools.ScarabRequestTool;
+import org.tigris.scarab.om.ScarabUser;
+
+/**
+ *  This is a helper class that extends TemplateAction to add
+ *  a couple methods useful for Scarab.
+ *   
+ *  @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
+ *  @version $Id$
+ */
+public abstract class ScarabTemplateAction extends TemplateAction
+{
+    private static final Category log = 
+        Category.getInstance("org.tigris.scarab");
+
+    protected static final String ERROR_MESSAGE = 
+        "More information was required to submit your request. Please " +
+        "scroll down to see error messages."; 
+
+    protected static final String NO_PERMISSION_MESSAGE = 
+         "You do not have permission to perform this action.";
+
+    protected static final String DEFAULT_MSG = "Your changes were saved.";             
+    protected static final String EMAIL_ERROR = ", but could not send " +
+        "notification email due to a mail server error.";
+
+    /**
+     * Helper method to retrieve the IntakeTool from the Context
+     */
+    public IntakeTool getIntakeTool(TemplateContext context)
+    {
+        return (IntakeTool) getTool(context, 
+                ScarabConstants.INTAKE_TOOL);
+    }
+
+    /**
+     * Helper method to retrieve the ScarabRequestTool from the Context
+     */
+    public ScarabRequestTool getScarabRequestTool(TemplateContext context)
+    {
+        return (ScarabRequestTool)context
+            .get(ScarabConstants.SCARAB_REQUEST_TOOL);
+    }
+
+    /**
+     * Returns the current template that is being executed, otherwisse
+     * it returns null
+     */
+    public String getCurrentTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.TEMPLATE, null);
+    }
+
+    /**
+     * Returns the current template that is being executed, otherwisse
+     * it returns defaultValue.
+     */
+    public String getCurrentTemplate(RunData data, String defaultValue)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.TEMPLATE, defaultValue);
+    }
+
+    /**
+     * Returns the nextTemplate to be executed. Otherwise returns null.
+     */
+    public String getNextTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.NEXT_TEMPLATE, null);
+    }
+
+    /**
+     * Returns the nextTemplate to be executed. Otherwise returns defaultValue.
+     */
+    public String getNextTemplate(RunData data, String defaultValue)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.NEXT_TEMPLATE, defaultValue);
+    }
+
+    /**
+     * Returns the last template to be cancelled back to.
+     */
+    public String getLastTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.LAST_TEMPLATE, null);
+    }
+
+    /**
+     * Returns the cancelTemplate to be executed. Otherwise returns null.
+     */
+    public String getCancelTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.CANCEL_TEMPLATE, null);
+    }
+
+    /**
+     * Returns the cancelTemplate to be executed. 
+     * Otherwise returns defaultValue.
+     */
+    public String getCancelTemplate(RunData data, String defaultValue)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.CANCEL_TEMPLATE, 
+                              defaultValue);
+    }
+
+    /**
+     * Returns the backTemplate to be executed. Otherwise returns null.
+     */
+    public String getBackTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.BACK_TEMPLATE, null);
+    }
+
+    /**
+     * Returns the backTemplate to be executed. 
+     * Otherwise returns defaultValue.
+     */
+    public String getBackTemplate(RunData data, String defaultValue)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.BACK_TEMPLATE, defaultValue);
+    }
+
+    /**
+     * Returns the other template that is being executed, otherwise
+     * it returns null.
+     */
+    public String getOtherTemplate(RunData data)
+    {
+        return data.getParameters()
+                   .getString(ScarabConstants.OTHER_TEMPLATE);
+    }
+
+    public void doSave( RunData data, TemplateContext context )
+        throws Exception
+    {
+    }
+
+    public void doGonext( RunData data, TemplateContext context )
+        throws Exception
+    {
+        setTarget(data, getNextTemplate(data));            
+    }
+
+    public void doGotoothertemplate( RunData data, 
+                                     TemplateContext context )
+        throws Exception
+    {
+        setTarget(data, getOtherTemplate(data));            
+    }
+
+    public void doRefresh( RunData data, TemplateContext context )
+        throws Exception
+    {
+        setTarget(data, getCurrentTemplate(data));            
+    }
+
+    public void doReset( RunData data, TemplateContext context )
+        throws Exception
+    {
+        IntakeTool intake = getIntakeTool(context);
+        intake.removeAll();
+        setTarget(data, getCurrentTemplate(data));            
+    }
+        
+    public void doCancel( RunData data, TemplateContext context )
+        throws Exception
+    {
+        setTarget(data, getCancelTemplate(data));            
+    }
+
+    public void doDone( RunData data, TemplateContext context )
+        throws Exception
+    {
+        doSave( data, context);
+        doCancel( data, context);
+    }
+
+
+    protected Category log()
+    {
+        return log;
+    }
+}
