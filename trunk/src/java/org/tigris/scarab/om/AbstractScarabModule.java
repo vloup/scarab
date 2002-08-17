1feee50d557aa9a5c7f51ea32049963f39151c85
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-08-17 00:15:26 +0000

    separate the getActiveAttributesBy Name out by user and non-user attributes.
    git-svn-id: http://scarab.tigris.org/svn/scarab@5434 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
+++ trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
@@ -1325,7 +1325,8 @@ public abstract class AbstractScarabModule
      * gets a list of all of the active Attributes.
      * ordered by name
      */
-    public List getActiveAttributesByName(IssueType issueType)
+    public List getActiveAttributesByName(IssueType issueType,
+                                          String attributeType)
         throws Exception
     {
         Criteria crit = new Criteria();
@@ -1336,6 +1337,19 @@ public abstract class AbstractScarabModule
                      AttributePeer.ATTRIBUTE_ID);
         crit.add(AttributePeer.DELETED, false);
         crit.add(RModuleAttributePeer.ACTIVE, true);
+        if (USER.equals(attributeType))
+        {
+            crit.add(AttributePeer.ATTRIBUTE_TYPE_ID, 
+                     AttributeTypePeer.USER_TYPE_KEY);
+            crit.addJoin(AttributePeer.ATTRIBUTE_ID,
+                     RModuleAttributePeer.ATTRIBUTE_ID); 
+        }
+        else if (NON_USER.equals(attributeType))
+        {
+            crit.add(AttributePeer.ATTRIBUTE_TYPE_ID, 
+                     AttributeTypePeer.USER_TYPE_KEY,
+                     Criteria.NOT_EQUAL);
+        }
         crit.addAscendingOrderByColumn(
                 RModuleAttributePeer.DISPLAY_VALUE);
         return AttributePeer.doSelect(crit);
