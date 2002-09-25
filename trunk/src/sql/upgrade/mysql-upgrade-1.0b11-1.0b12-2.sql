31755964dad5286e88415a149bdbfc7a9f6bbad4
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-09-25 23:57:17 +0000

    added docs
    git-svn-id: http://scarab.tigris.org/svn/scarab@5798 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/sql/upgrade/mysql-upgrade-1.0b11-1.0b12-2.sql
+++ trunk/src/sql/upgrade/mysql-upgrade-1.0b11-1.0b12-2.sql
@@ -1,2 +1,9 @@
-# Add columns to ISSUE_TYPE
+/*
+ * Add columns to ISSUE_TYPE
+ *
+ * $Id$
+ *
+ * Created By: Elicia David
+ */
+
 ALTER TABLE SCARAB_ISSUE_TYPE ADD ISDEFAULT INTEGER (1) default 0;
