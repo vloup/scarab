8db8be7ae0c399393527055799c038ecdeb56421
thierrylach <thierrylach@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-09-27 15:16:18 +0000

    Keep synchronized with original mysql.
    git-svn-id: http://scarab.tigris.org/svn/scarab@5810 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/sql/upgrade/oracle-upgrade-1.0b11-1.0b12-2.sql
+++ trunk/src/sql/upgrade/oracle-upgrade-1.0b11-1.0b12-2.sql
@@ -1,2 +1,10 @@
-/* Add columns to ISSUE_TYPE */
+/*
+ * Add columns to ISSUE_TYPE
+ *
+ * $Id$
+ *
+ * Created By: Elicia David
+ * Modified for Oracle By: Thierry Lach
+ */
+
 ALTER TABLE SCARAB_ISSUE_TYPE ADD ISDEFAULT NUMBER (1) default 0;
