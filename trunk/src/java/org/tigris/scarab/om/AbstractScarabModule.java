5efb6c25daf5b03f49e94225983b19ee7c663f0c
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-10-25 20:00:02 +0000

    add in module id when creating new group
    git-svn-id: http://scarab.tigris.org/svn/scarab@6122 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
+++ trunk/src/java/org/tigris/scarab/om/AbstractScarabModule.java
@@ -358,6 +358,7 @@ public abstract class AbstractScarabModule
         // Make default group name 'attribute group x' where x is size + 1
         ag.setName("Attribute group " + Integer.toString(groups.size()+1));
         ag.setActive(true);
+        ag.setModuleId(getModuleId());
         ag.setIssueTypeId(issueType.getIssueTypeId());
         if (groups.size() == 0)
         {
