369f4f8bd86599691b9d09c6d682da95ec43ffa0
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-12-02 16:45:43 +0000

    column ordering selected value was not getting matched with the correct attribute - SCB392.  I also simplified the UI so that the choice of whether to show the column is made with the same select that orders the attributes.
    git-svn-id: http://scarab.tigris.org/svn/scarab@6476 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/ConfigureIssueList.java
+++ trunk/src/java/org/tigris/scarab/actions/ConfigureIssueList.java
@@ -86,16 +86,12 @@ public class ConfigureIssueList extends RequireLoginFirstAction
         // Add user's new selection of attributes
         ParameterParser params = data.getParameters();
         String[] ids = params.getStrings("attid");
-        if (ids == null || ids.length == 0) 
+        String[] orders = params.getStrings("attorder");
+        List attributes = new ArrayList(ids.length);
+        final Map orderMap = new HashMap();            
+        for (int i =0; i<ids.length; i++)
         {
-            scarabR.setAlertMessage(l10n.get("MustSelectAtLeastOneAttribute"));
-        }
-        else
-        {
-            String[] orders = params.getStrings("attorder");
-            final Map orderMap = new HashMap();
-            List attributes = new ArrayList(ids.length);
-            for (int i =0; i<ids.length; i++)
+            if (!orders[i].equals("hidden")) 
             {
                 Attribute attribute = AttributeManager
                     .getInstance(new NumberKey(ids[i]));
@@ -103,6 +99,14 @@ public class ConfigureIssueList extends RequireLoginFirstAction
                 Integer order = new Integer(orders[i]);
                 orderMap.put(attribute, order);
             }
+        }
+
+        if (attributes.isEmpty())
+        {
+            scarabR.setAlertMessage(l10n.get("MustSelectAtLeastOneAttribute"));
+        }
+        else
+        {
             Comparator c = new Comparator()
                 {
                     public int compare(Object o1, Object o2)
