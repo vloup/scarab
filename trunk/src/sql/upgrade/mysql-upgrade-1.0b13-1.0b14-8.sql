49b434ff9c50483a6b0f0bafdcd34f66c8fa7857
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-12-20 05:35:07 +0000

    make sure the id_table is set to after the last entry
    git-svn-id: http://scarab.tigris.org/svn/scarab@6675 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-8.sql
+++ trunk/src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-8.sql
@@ -31,7 +31,13 @@ insert into xxxx_SCARAB_DEPEND (OBSERVED_ID, OBSERVER_ID, DEPEND_TYPE_ID, DELETE
     select OBSERVED_ID, OBSERVER_ID, DEPEND_TYPE_ID, DELETED
         from SCARAB_DEPEND;
 
+/* 
+ * remove the autoincrement and update the id_table to be after the last entry
+ */
 alter table xxxx_SCARAB_DEPEND modify DEPEND_ID integer NOT NULL; 
+delete from ID_TABLE where table_name='SCARAB_DEPEND';
+insert into ID_TABLE (id_table_id, table_name, next_id, quantity) select 28, 'SCARAB_DEPEND', count(*) + 1, 10 from xxxx_SCARAB_DEPEND;
+
 
 drop table SCARAB_DEPEND;
 
