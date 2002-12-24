7654f5323442fa4786e7d8cd7d8e86ace4da8edb
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-12-24 22:54:32 +0000

    this should fix scb429
    the problem was that with a new ActivitySet being passed into the issue.do*
    methods it was not null and thus was not being created...
    
    elicia, please test further.
    
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@6704 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/AssignIssue.java
+++ trunk/src/java/org/tigris/scarab/actions/AssignIssue.java
@@ -203,7 +203,7 @@ public class AssignIssue extends BaseModifyIssue
                 item.add(su);
                 userList.remove(item);
 
-                String newKey = "asso_user_{" + userId + "}_issue_{" + issueId + "}";
+                String newKey = "asso_user_{" + userId + "}_issue_{" + issueId + '}';
                 String newAttrId = params.get(newKey);
                 Attribute newAttribute = AttributeManager
                      .getInstance(new NumberKey(newAttrId));
@@ -231,7 +231,7 @@ public class AssignIssue extends BaseModifyIssue
         ScarabUser assigner = (ScarabUser)data.getUser();
         String reason = data.getParameters().getString("reason", "");
         Attachment attachment = null;
-        ActivitySet activitySet = new ActivitySet();
+        ActivitySet activitySet = null;
 
         for (int i=0; i < issues.size(); i++)
         {
@@ -336,9 +336,7 @@ public class AssignIssue extends BaseModifyIssue
                     }
                 }
             }
-                
         }
-
         
         if (scarabR.getAlertMessage() == null || 
             scarabR.getAlertMessage().toString().length() == 0)
