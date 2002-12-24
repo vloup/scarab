467cd02204582406bd0b13f9c0fb4cdcf626febc
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2002-12-24 12:31:31 +0000

    i'm still not 100% sure how END_DATE works (whether it applies to more than just option and user attributes), but the logic the way it was before was that ALL of the Activites in the database would be updated if it was not an option/user attribute...this should fix it to only execute on option/user attributes. i still think that this needs some serious review...john is going to have to do it since i'm not sure what the final result should be...
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@6702 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/Activity.java
+++ trunk/src/java/org/tigris/scarab/om/Activity.java
@@ -160,15 +160,16 @@ public class Activity
                 {
                    crit.add(ActivityPeer.NEW_OPTION_ID, this.getOldOptionId());
                 }
-            }
-            List result = ActivityPeer.doSelect(crit);
-            if (result.size() > 0)
-            {
-                for (int i=0; i<result.size();i++)
+                List result = ActivityPeer.doSelect(crit);
+                int resultSize = result.size();
+                if (resultSize > 0)
                 {
-                    Activity a = (Activity)result.get(i);
-                    a.setEndDate(getActivitySet().getCreatedDate());
-                    a.save(dbCon);
+                    for (int i=0; i<resultSize;i++)
+                    {
+                        Activity a = (Activity)result.get(i);
+                        a.setEndDate(getActivitySet().getCreatedDate());
+                        a.save(dbCon);
+                    }
                 }
             }
         }
