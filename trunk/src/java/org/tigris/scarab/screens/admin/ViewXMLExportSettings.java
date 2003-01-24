79cdc3fbd0bec6dcd74e2d788c9759c3a3f60de8
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-01-24 00:07:35 +0000

    move to a different output method because data.getOut() can sometimes be null.
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@6911 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/screens/admin/ViewXMLExportSettings.java
+++ trunk/src/java/org/tigris/scarab/screens/admin/ViewXMLExportSettings.java
@@ -111,7 +111,8 @@ public class ViewXMLExportSettings extends Default
         context.put("sdf", new SimpleDateFormat(ScarabConstants.DATE_FORMAT));
         String result = 
             Module.handleRequest (context, "macros/XMLExportSettingsMacro.vm");
-        data.getOut().write(result);
+        data.getResponse().setContentLength(result.length());
+        data.getResponse().getOutputStream().print(result);
         context.remove("renderedFromScreen");
 
         // we already sent the response, there is no target to render
