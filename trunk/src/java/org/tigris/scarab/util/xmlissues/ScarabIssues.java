7bf57d8de2ff328ca4ccccbfb3b06d163bb806ca
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-01-29 01:12:12 +0000

    if getReconcilePath == true, then validate the file exists and then copy it to the right place once the issue is created.
    14398
    
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@6952 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/util/xmlissues/ScarabIssues.java
+++ trunk/src/java/org/tigris/scarab/util/xmlissues/ScarabIssues.java
@@ -52,6 +52,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
 
 import java.text.SimpleDateFormat;
 
@@ -416,8 +417,16 @@ public class ScarabIssues implements java.io.Serializable
                 {
                     importUsers.add(activity.getNewUser());
                 }
-                if (activity.getAttachment() != null)
+                Attachment activityAttachment = activity.getAttachment();
+                if (activityAttachment != null)
                 {
+                    if (activityAttachment.getReconcilePath() &&
+                       ! new File(activityAttachment.getFilename()).exists())
+                    {
+                        importErrors.add("Could not find file attachment: " + 
+                            activityAttachment.getFilename());
+                    }
+
                     importUsers.add(activity.getAttachment().getCreatedBy());
                 }
 
@@ -704,6 +713,17 @@ public class ScarabIssues implements java.io.Serializable
                     {
                         activityAttachmentOM = createAttachment(issueOM, module, activityAttachment);
                         activityAttachmentOM.save();
+                        
+                        // Special case. After the Attachment object has been saved,
+                        // if the ReconcilePath == true, then assume that the fileName is 
+                        // an absolute path to a file and copy it to the right directory
+                        // structure under Scarab's path.
+                        if (activityAttachment.getReconcilePath())
+                        {
+                            activityAttachmentOM
+                                .copyFileFromTo(activityAttachment.getFilename(), 
+                                                activityAttachmentOM.getFullPath());
+                        }
                         log.debug("Created Activity Attachment object");
                     }
                 }
