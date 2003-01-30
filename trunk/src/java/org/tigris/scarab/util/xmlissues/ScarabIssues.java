d16890b6376ad28cc167b581dfb68d66584a2cb6
jon <jon@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-01-30 20:45:40 +0000

    make sure to check for null before adding the user.
    -jon
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@6963 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/util/xmlissues/ScarabIssues.java
+++ trunk/src/java/org/tigris/scarab/util/xmlissues/ScarabIssues.java
@@ -385,10 +385,17 @@ public class ScarabIssues implements java.io.Serializable
         for (Iterator itr = activitySets.iterator(); itr.hasNext();)
         {
             ActivitySet activitySet = (ActivitySet) itr.next();
-            importUsers.add(activitySet.getCreatedBy());
+            if (activitySet.getCreatedBy() != null)
+            {
+                importUsers.add(activitySet.getCreatedBy());
+            }
             if (activitySet.getAttachment() != null)
             {
-                importUsers.add(activitySet.getAttachment().getCreatedBy());
+                String attachCreatedBy = activitySet.getAttachment().getCreatedBy();
+                if (attachCreatedBy != null)
+                {
+                    importUsers.add(attachCreatedBy);
+                }
             }
             
             // validate the activity set types
@@ -427,7 +434,11 @@ public class ScarabIssues implements java.io.Serializable
                             activityAttachment.getFilename());
                     }
 
-                    importUsers.add(activity.getAttachment().getCreatedBy());
+                    String attachCreatedBy = activityAttachment.getCreatedBy();
+                    if (attachCreatedBy != null)
+                    {
+                        importUsers.add(attachCreatedBy);
+                    }
                 }
 
                 // Get the Attribute associated with the Activity
