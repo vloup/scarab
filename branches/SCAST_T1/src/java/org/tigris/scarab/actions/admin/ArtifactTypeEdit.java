cf72c3f05682d910278373f97f5633717d86e6e0
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-02-24 23:30:53 +0000

    *ArtifactTypeEdit.java: Cannot set the display name of an issue type to a name that already exists in this module. Do not save other widgets on page if name check fails. Issue number:  15073
    git-svn-id: http://scarab.tigris.org/svn/scarab@7222 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCAST_T1/src/java/org/tigris/scarab/actions/admin/ArtifactTypeEdit.java
+++ branches/SCAST_T1/src/java/org/tigris/scarab/actions/admin/ArtifactTypeEdit.java
@@ -104,9 +104,35 @@ public class ArtifactTypeEdit extends RequireLoginFirstAction
                                         rmit.getQueryKey(), false);
         if (intake.isAllValid())
         {
-            rmitGroup.setProperties(rmit);
-            rmit.save();
-            scarabR.setConfirmMessage(l10n.get(DEFAULT_MSG));  
+            boolean nameTaken = false;
+            List issueTypes = module.getRModuleIssueTypes();
+            if (issueTypes != null)
+            {
+                String displayName = rmitGroup.get("DisplayName").toString();
+                for (int i=0;i<issueTypes.size();i++)
+                {
+                    RModuleIssueType tmpRmit = ((RModuleIssueType)issueTypes.get(i));
+                    if (tmpRmit.getDisplayName().equals(displayName) 
+                        && !tmpRmit.getIssueTypeId().equals(issueType.getIssueTypeId()))
+                    {
+                        nameTaken = true;
+                        break;
+                    }
+                }
+            }
+         
+            if (nameTaken) 
+            {
+                scarabR.setAlertMessage(l10n.get(ERROR_MESSAGE));
+                rmitGroup.get("DisplayName").setMessage("IssueTypeNameExists");
+                return false;
+            }
+            else
+            {
+                rmitGroup.setProperties(rmit);
+                rmit.save();
+                scarabR.setConfirmMessage(l10n.get(DEFAULT_MSG));  
+            }
         }
         else
         {
@@ -482,9 +508,16 @@ public class ArtifactTypeEdit extends RequireLoginFirstAction
     public void doDone(RunData data, TemplateContext context)
         throws Exception
     {
+        boolean groupSuccess = false;
         boolean infoSuccess = doSaveinfo(data, context);
-        boolean groupSuccess = doSavegroups(data, context);
-        doSaveuserattributes(data, context);
+        if (infoSuccess)
+        {
+            groupSuccess = doSavegroups(data, context);
+            if (groupSuccess)
+            {
+                doSaveuserattributes(data, context);
+            }
+        }
         if (infoSuccess && groupSuccess)
         {
             doCancel(data, context);
