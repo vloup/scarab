143f8e7a34078a5f81c397d4c5d88f1409f1a80e
elicia <elicia@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-02-26 20:32:44 +0000

    * Issue.java: use iterator pattern Issue number:  14981
    git-svn-id: http://scarab.tigris.org/svn/scarab@7259 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCAST_T1/src/java/org/tigris/scarab/om/Issue.java
+++ branches/SCAST_T1/src/java/org/tigris/scarab/om/Issue.java
@@ -2182,24 +2182,18 @@ public class Issue
             // Adjust dependencies if its a new issue id
             // (i.e.. moved to new module)
             List children = getChildren();
-            if (children != null && children.size() > 0)
+            for (Iterator i = children.iterator(); i.hasNext();)
             {
-                for (int i=0; i<children.size(); i++)
-                {
-                     Depend depend = (Depend)children.get(i);
-                     depend.setObservedId(newIssue.getIssueId());
-                     depend.save();
-                }
+                 Depend depend = (Depend)i.next();
+                 depend.setObservedId(newIssue.getIssueId());
+                 depend.save();
             }
             List parents = getParents();
-            if (parents != null && parents.size() > 0)
+            for (Iterator j = parents.iterator(); j.hasNext();)
             {
-                for (int i=0; i<parents.size(); i++)
-                {
-                     Depend depend = (Depend)parents.get(i);
-                     depend.setObserverId(newIssue.getIssueId());
-                     depend.save();
-                }
+                 Depend depend = (Depend)j.next();
+                 depend.setObserverId(newIssue.getIssueId());
+                 depend.save();
             }
             // Save activitySet record
             ActivitySet activitySet = ActivitySetManager
