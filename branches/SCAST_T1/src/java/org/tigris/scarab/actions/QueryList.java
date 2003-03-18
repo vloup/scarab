2fc9d582ad6424a6eadffcd57905ef7fc588ec0c
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-03-18 01:04:15 +0000

    Issue number: 14783 - Choice of Default query on QueryList.vm is not persistent Modified Files: * src/java/org/tigris/scarab/actions/QueryList.java - save RQueryUser   independent of whether there was any subscription fields (which are not   implemented).
    git-svn-id: http://scarab.tigris.org/svn/scarab@7377 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCAST_T1/src/java/org/tigris/scarab/actions/QueryList.java
+++ branches/SCAST_T1/src/java/org/tigris/scarab/actions/QueryList.java
@@ -91,6 +91,7 @@ public class QueryList extends RequireLoginFirstAction
        
         if (intake.isAllValid())
         {
+            boolean valid = true;
             List queries = scarabR.getAllQueries();
             for (int i = 0; i < queries.size(); i++)
             {    
@@ -103,15 +104,16 @@ public class QueryList extends RequireLoginFirstAction
                 {
                     Field freq = queryGroup.get("Frequency");
                     freq.setRequired(true);
-                    if (freq.isValid())
+                    if (!freq.isValid())
                     {
-                       queryGroup.setProperties(rqu);
-                       rqu.save();
+                        valid = false;
+                        freq.setMessage("EnterSubscriptionFrequency");
                     }
-                    else
-                    {
-                       freq.setMessage("EnterSubscriptionFrequency");
-                    }
+                }                
+                if (valid) 
+                {
+                    queryGroup.setProperties(rqu);
+                    rqu.save();
                 }
             }
        }
