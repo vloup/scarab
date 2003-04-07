fe3f160907ab974d161677951606b8f6c78c3f2c
jmcnally <jmcnally@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-04-07 00:24:24 +0000

    Forward port from SCAST_T1 Issue number: 16453 - Artifact type is corrupted.  Modified Files:  * src/java/org/tigris/scarab/om/Attribute.java - when deleting an attribute    make sure to delete the RAttributeAttributeGroup entries as well as the    RModuleAttribute and RIssueTypeAttribute entries.    Also contains one improvement to use iterator over random access of list.
    git-svn-id: http://scarab.tigris.org/svn/scarab@7583 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/Attribute.java
+++ trunk/src/java/org/tigris/scarab/om/Attribute.java
@@ -49,6 +49,7 @@ package org.tigris.scarab.om;
 // JDK classes
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
@@ -713,6 +714,18 @@ public class Attribute
         throws Exception
     {
         Criteria crit = new Criteria();
+        crit.add(RAttributeAttributeGroupPeer.ATTRIBUTE_ID,
+                 getAttributeId());
+        crit.addJoin(RAttributeAttributeGroupPeer.GROUP_ID,
+                     AttributeGroupPeer.ATTRIBUTE_GROUP_ID);
+        crit.add(AttributeGroupPeer.MODULE_ID, (Object)null, Criteria.NOT_EQUAL);
+        List raags = RAttributeAttributeGroupPeer.doSelect(crit);
+        for (Iterator i = raags.iterator(); i.hasNext();)
+        {
+            ((RAttributeAttributeGroup)i.next()).delete(user);
+        }
+
+        crit = new Criteria();
         crit.add(RModuleAttributePeer.ATTRIBUTE_ID, 
                  getAttributeId());
         List rmas = RModuleAttributePeer.doSelect(crit);
@@ -731,14 +744,26 @@ public class Attribute
         throws Exception
     {
         Criteria crit = new Criteria();
+        crit.add(RAttributeAttributeGroupPeer.ATTRIBUTE_ID,
+                 getAttributeId());
+        crit.addJoin(RAttributeAttributeGroupPeer.GROUP_ID,
+                     AttributeGroupPeer.ATTRIBUTE_GROUP_ID);
+        crit.add(AttributeGroupPeer.MODULE_ID, null);
+        List raags = RAttributeAttributeGroupPeer.doSelect(crit);
+        for (Iterator i = raags.iterator(); i.hasNext();)
+        {
+            ((RAttributeAttributeGroup)i.next()).delete(user);
+        }
+
+        crit = new Criteria();
         crit.add(RIssueTypeAttributePeer.ATTRIBUTE_ID, 
                  getAttributeId());
         List rias = RIssueTypeAttributePeer.doSelect(crit);
-        for (int i=0; i<rias.size(); i++)
+        for (Iterator i = rias.iterator(); i.hasNext();)
         {
-            RIssueTypeAttribute ria = (RIssueTypeAttribute)rias.get(i);
-            ria.delete(user);
+            ((RIssueTypeAttribute)i.next()).delete(user);
         }
+        
         ScarabCache.clear();
     }
 }
