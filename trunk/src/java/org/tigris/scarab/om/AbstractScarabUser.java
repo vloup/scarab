a80f276e04f919dec767d5d9c3ba0ab9a1538e10
dlr <dlr@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-04-10 21:00:44 +0000

    * src/java/org/tigris/scarab/om/AbstractScarabUser.java   getHomePage(Module): Handle the case where a user doesn't have an ID   (e.g. for anonymous user used during password expiration).  Improved   error handling and used the Index.vm default across the board.   Fixes stack trace seen when a user's password expires.
    git-svn-id: http://scarab.tigris.org/svn/scarab@7611 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/AbstractScarabUser.java
+++ trunk/src/java/org/tigris/scarab/om/AbstractScarabUser.java
@@ -718,22 +718,28 @@ public abstract class AbstractScarabUser
         throws TorqueException
     {
         String homePage = null;
-        UserPreference up = UserPreferenceManager.getInstance(getUserId());
-        homePage = up.getHomePage();
-        int i=0;
-        while (homePage == null || !isHomePageValid(homePage, module)) 
+        try
         {
-            try
+            // A user with no id won't have preferences.  The
+            // anonymous user used during password expiration (or an
+            // unsaved user) would exhibit this behavior.
+            Integer uid = getUserId();
+            if (uid != null)
             {
-                homePage = HOME_PAGES[i++];
-            }
-            catch (Exception e)
-            {
-                homePage = "Index.vm";
-                Log.get().warn("Error determining user homepage.", e);
+                UserPreference up = UserPreferenceManager.getInstance(uid);
+                homePage = up.getHomePage();
+                int i = 0;
+                while (homePage == null || !isHomePageValid(homePage, module)) 
+                {
+                    homePage = HOME_PAGES[i++];
+                }
             }
         }
-        return homePage;
+        catch (Exception e)
+        {
+            Log.get().warn("Error determining user homepage", e);
+        }
+        return (homePage != null ? homePage : "Index.vm");
     }
 
     /**
