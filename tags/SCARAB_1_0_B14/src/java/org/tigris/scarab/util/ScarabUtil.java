a5b704ade0fca957a7f4cc5b742b29c9bcbe615d
(no author) <(no author)@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-05-03 01:27:26 +0000

    This commit was manufactured by cvs2svn to create tag 'SCARAB_1_0_B14'.
    git-svn-id: http://scarab.tigris.org/svn/scarab@7853 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/SCARAB_1_0_B14/src/java/org/tigris/scarab/util/ScarabUtil.java
@@ -0,0 +1,198 @@
+package org.tigris.scarab.util;
+
+/* ================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */ 
+
+import java.util.List;
+import java.util.Iterator;
+
+import org.apache.oro.text.perl.Perl5Util;
+
+import org.tigris.scarab.om.Module;
+import org.tigris.scarab.util.IssueIdParser;
+
+/**
+ * A Utility class for code that doesn't really go other places.
+ *   
+ * @author <a href="mailto:jon@collab.net">Jon Scott Stevens</a>
+ * @version $Id$
+ */
+public class ScarabUtil
+{
+    private static final String REGEX_URL =
+        "s%\\b(?:[hH][tT]{2}[pP]|[fF][tT][pP]):[^ \\t\\n<>\"]+[\\w/]*%<a href=\"$0\">$0</a>%g";
+    private static final String REGEX_MAILTO =
+        "s%\\b(?:([mM][aA][iI][lL][tT][oO])):([^ \\t\\n<>\"]+[\\w/])*%<a href=\"$0\">$2</a>%g";
+    private static final String REGEX_NEWLINETOBR =
+        "s%\\n%<br />%g";
+
+    private static Perl5Util perlUtil = new Perl5Util();
+
+    /**
+     * First, it converts all HTML markup into entities.
+     * Then it the Jakarta ORO package to convert http:// ftp:// mailto: 
+     * links into URL's.
+     * Lastly, it uses the IssueIdParser to convert all issue id's into links.
+     */
+    public static String linkifyText(String input, ScarabLink link, Module module)
+        throws Exception
+    {
+        StringBuffer sb = new StringBuffer(input.length() * 2);
+        // first get rid of any HTML crap
+        String output = ReferenceInsertionFilter.filter(input);
+        output = perlUtil.substitute(REGEX_NEWLINETOBR,output);
+        output = perlUtil.substitute(REGEX_MAILTO,output);
+        output = perlUtil.substitute(REGEX_URL,output);
+        List result = IssueIdParser.tokenizeText(module, output);
+        for (Iterator itr = result.iterator(); itr.hasNext();)
+        {
+            Object tmp = itr.next();
+            if (tmp instanceof String)
+            {
+                sb.append(tmp);
+            }
+            else
+            {
+                List tmpList = (List)tmp;
+                link.addPathInfo("id", (String)tmpList.get(1));
+                link.setLabel((String)tmpList.get(0));
+                String bar = link.setAlternateText((String)tmpList.get(0)).toString();
+                sb.append(bar);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * URL encodes <code>in</code> and writes it to <code>out</code>. If the
+     * string is null, 'null' will be written. Code 'borrowed' from DynamicURI.java
+     * in the Jakarta Turbine 3 package. We use this code instead of java.net.Encoder
+     * because Encoder.encode is deprecated and we don't feel like putting a dependency
+     * on JDK 1.4.1. This should work fine for our purposes.
+     *
+     * @param in String to write.
+     * @param out Buffer to write to. Null if nothing in in.
+     */
+    public static final String urlEncode(String in)
+    {
+        if (in == null || in.length() == 0)
+        {
+            return null;
+        }
+
+        StringBuffer out = new StringBuffer(in.length()+128);
+        // This is the most expensive operation:
+        byte[] bytes = in.getBytes();
+
+        for (int i = 0; i < bytes.length; i++)
+        {
+            char c = (char) bytes[i];
+
+            if ( c < 128 && safe[ c ] )
+            {
+                out.append(c);
+            }
+            else if (c == ' ')
+            {
+                out.append('+');
+            }
+            else
+            {
+                byte toEscape = bytes[i];
+                out.append('%');
+                int low = (int) (toEscape & 0x0f);
+                int high = (int) ((toEscape & 0xf0) >> 4);
+                out.append(HEXADECIMAL[high]);
+                out.append(HEXADECIMAL[low]);
+            }
+        }
+        return out.toString();
+    }
+
+    // ------------------------------------- private constants for url encoding
+
+    /**
+     * Array mapping hexadecimal values to the corresponding ASCII characters.
+     */
+    private static final char[] HEXADECIMAL =
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F'
+        };
+
+    /**
+     * Characters that need not be encoded. This is much faster than using a
+     * BitSet, and for such a small array the space cost seems justified.
+     */
+    private static boolean[] safe = new boolean[ 128 ];
+
+    /** Static initializer for {@link #safe} */
+    static
+    {
+        for (int i = 'a'; i <= 'z'; i++)
+        {
+            safe[ i ] = true;
+        }
+        for (int i = 'A'; i <= 'Z'; i++)
+        {
+            safe[ i ] = true;
+        }
+        for (int i = '0'; i <= '9'; i++)
+        {
+            safe[ i ] = true;
+        }
+
+        safe['-'] = true;
+        safe['_'] = true;
+        safe['.'] = true;
+        safe['!'] = true;
+        safe['~'] = true;
+        safe['*'] = true;
+        safe['\''] = true;
+        safe['('] = true;
+        safe[')'] = true;
+    }
+}
---
+++ tags/SCARAB_1_0_B14/src/java/org/tigris/scarab/util/xmlissues/ImportIssues.java
@@ -0,0 +1,484 @@
+package org.tigris.scarab.util.xmlissues;
+
+/* ================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */
+
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import java.io.File;
+import java.io.Writer;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+
+import org.apache.commons.fileupload.FileItem;
+
+import org.apache.fulcrum.localization.Localization;
+import org.apache.commons.betwixt.XMLIntrospector;
+import org.apache.commons.betwixt.io.BeanReader;
+import org.apache.commons.betwixt.io.BeanWriter;
+import org.apache.commons.betwixt.strategy.HyphenatedNameMapper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.tigris.scarab.workflow.WorkflowFactory;
+import org.tigris.scarab.util.TurbineInitialization;
+import org.tigris.scarab.util.xmlissues.ScarabIssues;
+import org.tigris.scarab.util.ScarabConstants;
+
+import org.tigris.scarab.om.Module;
+
+
+/**
+ * This is a bean'ish object which allows one to set values for importing 
+ * issues, and then run the actual import. 
+ *
+ * Amenable to the ant task wrapper or you can pass an explicit file for 
+ * explicit import if turbine is already up and running.
+ * 
+ * <p>The way the ant task wrapper works is simple: call all the appropriate
+ * set methods to define the properties. Then you will need to call the init()
+ * and execute methods to start running things. Note: If Turbine is already
+ * initialized, there is no need to call the init() method.
+ *
+ * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
+ * @version $Id$
+ */
+public class ImportIssues
+{
+    private static final Log LOG = LogFactory.getLog(ImportIssues.class);
+
+
+    /** 
+     * Name of the properties file.
+     */
+    private String trProps = "/WEB-INF/conf/TurbineResourcesTest.properties";
+
+    /** 
+     * Name of the xmlimport.properties file used for configuration of log4j.
+     */
+    private String configProps = "/WEB-INF/conf/xmlimport.properties";
+
+    private File configDir = null;
+    private boolean sendEmail = false;
+    private File xmlFile = null;
+
+    /**
+     * Instance of scarabissues we ran the actual insert with.
+     *
+     * Make it available post import so importer can get at info about
+     * what has just been imported.
+     */
+    private ScarabIssues si = null;
+
+
+    public boolean getSendEmail()
+    {
+        return this.sendEmail;
+    }
+
+    public void setSendEmail(boolean state)
+    {
+        this.sendEmail = state;
+    }
+
+    public File getXmlFile()
+    {
+        return this.xmlFile;
+    }
+
+    public void setXmlFile(File xmlFile)
+    {
+        this.xmlFile = xmlFile;
+    }
+
+    public File getConfigDir()
+    {
+        return this.configDir;
+    }
+
+    public void setConfigDir(File configDir)
+    {
+        this.configDir = configDir;
+    }
+
+    public String getConfigFile()
+    {
+        return this.configProps;
+    }
+
+    public void setConfigFile(String configProps)
+    {
+        this.configProps = configProps;
+    }
+
+    public String getTurbineResources()
+    {
+        return this.trProps;
+    }
+
+    public void setTurbineResources(String trProps)
+    {
+        this.trProps = trProps;
+    }
+
+    public void init()
+        throws Exception
+    {
+        TurbineInitialization.setTurbineResources(getTurbineResources());
+        TurbineInitialization.setUp(getConfigDir().getAbsolutePath(), 
+            getConfigFile());
+    }
+
+    public void execute() 
+        throws Exception
+    {
+        runImport(getXmlFile());
+    }
+
+    /**
+     * Run an import.
+     *
+     * Assumes we're up and running inside of turbine.
+     *
+     * @param importFile File to import.
+     *
+     * @return List of errors if any.
+     *
+     * @exception Exception
+     */
+    public List runImport(File importFile)
+        throws Exception
+    {
+        return runImport(importFile, null);
+    }
+
+    /**
+     * Run an import.
+     *
+     * Assumes we're up and running inside of turbine.
+     *
+     * @param importFile File to import.
+     * @param currentModule If non-null, run check that import is going 
+     * against this module.
+     *
+     * @return List of errors if any.
+     *
+     * @exception Exception
+     */
+    public List runImport(File importFile, Module currentModule)
+        throws Exception
+    {
+        List importErrors = null;
+        LOG.debug("Importing issues from XML file: "
+                  + importFile.getAbsolutePath());
+
+        try
+        {
+            // Disable workflow
+            WorkflowFactory.setForceUseDefault(true);
+
+            BeanReader reader = createScarabIssuesBeanReader();
+            importErrors = validate(importFile.getAbsolutePath(), 
+                new BufferedInputStream(new FileInputStream(importFile)),
+                reader, currentModule);
+            if (importErrors == null)
+            {
+                this.si = insert(importFile.getAbsolutePath(), 
+                    new BufferedInputStream(new FileInputStream(importFile)),
+                    reader);
+            }
+        }
+        catch (Exception e)
+        {
+            LOG.error("Error importing issues from XML file: "
+                      + importFile.getAbsolutePath(), e);
+            throw e;
+        }
+        finally
+        {
+            // Renable workflow
+            WorkflowFactory.setForceUseDefault(false);
+        }
+
+        return importErrors;
+    }
+
+    /**
+     * Run an import.
+     *
+     * Assumes we're up and running inside of turbine.  Awkwardly duplicates 
+     * {@link #runImport(File) import} but duplication is so we can do the reget of
+     * the input stream; FileInput "manages" backing up the Upload for us on the
+     * second get of the input stream (It creates new ByteArrayInputStream 
+     * w/ the src being a byte array of the file its kept in memory or in 
+     * temporary storage on disk).  
+     *
+     * @param importFile FileItem reference to use importing.
+     *
+     * @return List of errors if any.
+     *
+     * @exception Exception
+     */
+    public List runImport(FileItem importFile)
+        throws Exception
+    {
+        return runImport(importFile, null);
+    }
+
+    /**
+     * Run an import.
+     *
+     * Assumes we're up and running inside of turbine.  Awkwardly duplicates 
+     * {@link #runImport(File) import} but duplication is so we can do the reget of
+     * the input stream; FileInput "manages" backing up the Upload for us on the
+     * second get of the input stream (It creates new ByteArrayInputStream 
+     * w/ the src being a byte array of the file its kept in memory or in 
+     * temporary storage on disk).  
+     *
+     * @param importFile FileItem reference to use importing.
+     * @param currentModule If non-null, run check that import is going 
+     * against this module.
+     *
+     * @return List of errors if any.
+     *
+     * @exception Exception
+     */
+    public List runImport(FileItem importFile, Module currentModule)
+        throws Exception
+    {
+        List importErrors = null;
+        LOG.debug("Importing issues from uploaded XML: "
+                  + importFile.getName());
+
+        try
+        {
+            // Disable workflow
+            WorkflowFactory.setForceUseDefault(true);
+            BeanReader reader = createScarabIssuesBeanReader();
+            importErrors = validate(importFile.getName(), 
+                importFile.getInputStream(), reader, currentModule);
+            if (importErrors == null)
+            {
+                // Reget the input stream.
+                this.si = insert(importFile.getName(), 
+                    importFile.getInputStream(), reader);
+            }
+        }
+        catch (Exception e)
+        {
+            LOG.error("Error importing issues from uploaded XML: "
+                      + importFile.getName(), e);
+            throw e;
+        }
+        finally
+        {
+            // Renable workflow
+            WorkflowFactory.setForceUseDefault(false);
+        }
+
+        return importErrors;
+    }
+
+    /**
+     * Run validation phase.
+     *
+     * @param name Filename to output in log message.  May be null.
+     * @param is Input stream to read.
+     * @param reader ScarabIssues bean reader instance.
+     * @param currentModule If non-null, run check that import is going 
+     * against this module.
+     *
+     * @return Null if stream passes validation else list of errors.
+     *
+     * @exception Exception.
+     */
+    protected List validate(String name, InputStream is, BeanReader reader, 
+            Module currentModule)
+        throws Exception
+    {
+        ScarabIssues.setInValidationMode(true);
+        ScarabIssues si = (ScarabIssues)reader.parse(is);
+        si.doValidateDependencies();
+        si.doValidateUsers();
+        List importErrors = si.doGetImportErrors();
+        if(currentModule != null)
+        {
+            // If currentModule is not null, make sure the xml module is that
+            // of the passed currentModule.  We do the check here late because
+            // we know the xml is good if we get this far -- that the 
+            // si.getModule() will not return null.
+            String xmlCode = si.getModule().getCode();
+            if (xmlCode == null || !currentModule.getCode().equals(xmlCode))
+            {
+                if (importErrors == null)
+                {
+                    importErrors = new ArrayList();
+                }
+
+                Object[] args = {si.getModule().getName(), currentModule.getName()};
+                String error = Localization.format(
+                    ScarabConstants.DEFAULT_BUNDLE_NAME,
+                    getLocale(),
+                    "XMLModuleNotCurrent", args);
+                importErrors.add(error);
+            }
+        }
+
+        if (importErrors != null) 
+        {
+            if (importErrors.size() == 0)
+            {
+                importErrors = null;
+            }
+            else
+            {
+                LOG.error("Found " + importErrors.size() + " errors importing "
+                    + ((name != null)? name: "null") + ":");
+                for (Iterator itr = importErrors.iterator(); itr.hasNext();)
+                {
+                    String message = (String)itr.next();
+                    LOG.error(message);
+                }
+            }
+        }
+
+        return importErrors;
+    }
+
+    /**
+     * Do actual issue insert.
+     *
+     * Assumes issues passed have already been validated.  If they haven't
+     * been, could damage scarab.
+     *
+     * @param name Name to use in log messages (E.g. filename).  May be null.
+     * @param is Input stream of xml to insert.
+     * @param reader ScarabIssues bean reader instance.
+     * 
+     * @return The instance of scarabissues we inserted in case you need to 
+     * display info about the issues inserted.
+     */
+    protected ScarabIssues insert(String name, InputStream is, 
+            BeanReader reader)
+        throws Exception
+    {
+        ScarabIssues.setInValidationMode(false);
+        ScarabIssues si = (ScarabIssues)reader.parse(is);
+        si.doHandleDependencies();
+        LOG.debug("Successfully imported " + name + "!");
+        return si;
+    }
+
+    /**
+     * Get instance of the ScarabIssues used importing.
+     *
+     * You'd use this method to get at the instance of scarab issues used 
+     * importing for case where you want to print out info on the import thats
+     * just happened. Call after a successful import. Calling before will give
+     * undefined results.
+     *
+     * @return Instance of ScarabIssues we ran the import with.
+     */
+    public ScarabIssues getScarabIssuesBeanReader()
+    {
+        return this.si;
+    }
+
+    /**
+     * Return a bean reader for ScarabIssue.
+     *
+     * @return A bean reader.
+     */
+    protected BeanReader createScarabIssuesBeanReader()
+        throws Exception
+    {
+        BeanReader reader = new BeanReader();
+        reader.setXMLIntrospector(createXMLIntrospector());
+        reader.registerBeanClass(ScarabIssues.class);
+        return reader;
+    }
+
+    protected XMLIntrospector createXMLIntrospector()
+    {
+        XMLIntrospector introspector = new XMLIntrospector();
+
+        // set elements for attributes to true
+        introspector.setAttributesForPrimitives(false);
+
+        // wrap collections in an XML element
+        //introspector.setWrapCollectionsInElement(true);
+
+        // turn bean elements into lower case
+        introspector.setElementNameMapper(new HyphenatedNameMapper());
+
+        return introspector;
+    }
+
+    /**
+     * Method to output the bean object as XML. 
+     * 
+     * Not used right now.
+     */
+    protected void write(Object bean, Writer out)
+        throws Exception
+    {
+        BeanWriter writer = new BeanWriter(out);
+        writer.setXMLIntrospector(createXMLIntrospector());
+        writer.enablePrettyPrint();
+        writer.setWriteIDs(false);
+        writer.write(bean);
+    }
+
+    private Locale getLocale()
+    {
+        return ScarabConstants.DEFAULT_LOCALE;
+    }
+}
