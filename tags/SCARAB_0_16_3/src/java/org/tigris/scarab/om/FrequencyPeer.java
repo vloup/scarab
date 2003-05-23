489717d3d82d25f50d20f005ec24f79bdd1dd9a9
(no author) <(no author)@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-05-23 23:32:09 +0000

    This commit was manufactured by cvs2svn to create tag 'SCARAB_0_16_3'.
    git-svn-id: http://scarab.tigris.org/svn/scarab@8014 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/SCARAB_0_16_3/src/java/org/tigris/scarab/om/FrequencyPeer.java
@@ -0,0 +1,23 @@
+package org.tigris.scarab.om;
+
+import java.util.List;
+import org.apache.torque.util.Criteria;
+
+/** 
+ *  You should add additional methods to this class to meet the
+ *  application requirements.  This class will only be generated as
+ *  long as it does not already exist in the output directory.
+ */
+public class FrequencyPeer 
+    extends org.tigris.scarab.om.BaseFrequencyPeer
+{
+
+    /**
+     * Returns list of all frequency values.
+     */
+    public static List getFrequencies() throws Exception
+    {
+        return doSelect(new Criteria());
+    }
+
+}
