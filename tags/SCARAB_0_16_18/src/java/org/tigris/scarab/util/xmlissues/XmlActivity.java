5460911cc1dfb931551e9203da086f0cf0ff8719
(no author) <(no author)@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-06-27 20:59:27 +0000

    This commit was manufactured by cvs2svn to create tag 'SCARAB_0_16_18'.
    git-svn-id: http://scarab.tigris.org/svn/scarab@8207 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/SCARAB_0_16_18/src/java/org/tigris/scarab/util/xmlissues/XmlActivity.java
@@ -0,0 +1,252 @@
+package org.tigris.scarab.util.xmlissues;
+
+/* ================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */ 
+
+public class XmlActivity implements java.io.Serializable
+{
+    private String id = null;
+    private String attribute = null;
+    private String oldNumericValue = null;
+    private String newNumericValue = null;
+    private String oldUser = null;
+    private String newUser = null;
+    private String oldOption = null;
+    private String newOption = null;
+    private String oldValue = null;
+    private String newValue = null;
+    private Dependency dependency = null;
+    private String description = null;
+    private EndDate endDate = null;
+    private XmlAttachment attachment = null;
+
+    private boolean isNewActivity = true;
+    
+    public XmlActivity()
+    {
+    }
+
+    public boolean isNewActivity()
+    {
+        return isNewActivity;
+    }
+
+    public boolean isRemoveUserActivity()
+    {
+        return (getNewUser() == null && getOldUser() != null);
+    }
+
+    public boolean isAddDependency()
+    {
+        return (getDependency() != null &&
+                getOldValue() == null &&
+                getNewValue() != null);
+    }
+
+    public boolean isDeleteDependency()
+    {
+        return (getDependency() != null &&
+                getOldValue() != null &&
+                getNewValue() == null);
+    }
+
+    public boolean isUpdateDependency()
+    {
+        return (getDependency() != null &&
+                getOldValue() != null &&
+                getNewValue() != null);
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public void setAttribute(String attribute)
+    {
+        this.attribute = attribute;
+    }
+
+    public String getAttribute()
+    {
+        return this.attribute;
+    }
+
+    public void setOldNumericValue(String oldNumericValue)
+    {
+        this.oldNumericValue = oldNumericValue;
+        isNewActivity = false;
+    }
+
+    public String getOldNumericValue()
+    {
+        return this.oldNumericValue;
+    }
+
+    public void setNewNumericValue(String newNumericValue)
+    {
+        this.newNumericValue = newNumericValue;
+    }
+
+    public String getNewNumericValue()
+    {
+        return this.newNumericValue;
+    }
+
+    public void setOldUser(String oldUser)
+    {
+        this.oldUser = oldUser;
+        isNewActivity = false;
+    }
+
+    public String getOldUser()
+    {
+        return this.oldUser;
+    }
+
+    public void setNewUser(String newUser)
+    {
+        this.newUser = newUser;
+    }
+
+    public String getNewUser()
+    {
+        return this.newUser;
+    }
+
+    public void setOldOption(String oldOption)
+    {
+        this.oldOption = oldOption;
+        isNewActivity = false;
+    }
+
+    public String getOldOption()
+    {
+        return this.oldOption;
+    }
+
+    public void setNewOption(String newOption)
+    {
+        this.newOption = newOption;
+    }
+
+    public String getNewOption()
+    {
+        return this.newOption;
+    }
+
+    public void setOldValue(String oldValue)
+    {
+        this.oldValue = oldValue;
+        isNewActivity = false;
+    }
+
+    public String getOldValue()
+    {
+        return this.oldValue;
+    }
+
+    public void setNewValue(String newValue)
+    {
+        this.newValue = newValue;
+    }
+
+    public String getNewValue()
+    {
+        return this.newValue;
+    }
+
+    public Dependency getDependency()
+    {
+        return dependency;
+    }
+    
+    public void setDependency(Dependency name)
+    {
+        this.dependency = name;
+        isNewActivity = true;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public EndDate getEndDate()
+    {
+        return endDate;
+    }
+    
+    public void setEndDate(EndDate endDate)
+    {
+        this.endDate = endDate;
+    }
+
+    public void setAttachment(XmlAttachment attachment)
+    {
+        this.attachment = attachment;
+    }
+
+    public XmlAttachment getAttachment()
+    {
+        return this.attachment;
+    }
+    
+    public String toString()
+    {
+        return ("Activity Id: " + id);
+    }
+}
