f038446f95e20eed31e4194be320bdbc370d8d8f
dlr <dlr@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-07-02 16:58:10 +0000

    Corrected error which occurs when trying to rename a new user attribute.
    * src/java/org/tigris/scarab/om/Attribute.java
      Corrected method JavaDoc formatting.
    
      hasModuleMappings(): Wrapped hasMapping(Module, IssueType).
    
      hasMapping(): Gated use of parameters, which may now be pased in as
      null.
    
      hasGlobalIssueTypeMappings(): Renamed from hasIssueTypeMappings().
      Wrapped hasGlobalMapping(IssueType).
    
      hasGlobalMapping(IssueType): Renamed from hasMapping(IssueType).
      Gated use of parameter, which may now be pased in as null.
    
    * src/java/org/tigris/scarab/actions/admin/GlobalAttributeEdit.java
      doSaveattributedata(): Renamed hasIssueTypeMappings() to
      hasGlobalIssueTypeMappings().
    
      mapAttribute(): Renamed hasMapping(IssueType) to
      hasGlobalMapping(IssueType).
    
    * src/templates/screens/admin/UserAttributeEdit.vm
    * src/templates/screens/admin/GlobalAttributeEdit.vm
      Renamed hasIssueTypeMappings() to hasGlobalIssueTypeMappings().
    
    
    Issue: PCN19675
    Submitted by: Daniel Rall, IRK, and John McNally
    Reviewed by: John McNally and Venkat
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@8213 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/actions/admin/GlobalAttributeEdit.java
+++ trunk/src/java/org/tigris/scarab/actions/admin/GlobalAttributeEdit.java
@@ -143,7 +143,8 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
                 // With modules or issue types, give confirmation.
                 if (!attr.getDeleted() && 
                     attrGroup.get("Deleted").toString().equals("true") &&
-                    (attr.hasModuleMappings() || attr.hasIssueTypeMappings()))
+                    (attr.hasModuleMappings() ||
+                     attr.hasGlobalIssueTypeMappings()))
                 {
                     context.put("deleting", "deleting");
                     confirmDelete=true;
@@ -504,7 +505,7 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
                                 currentModule.getModuleId());
                 }
                 // Add user attribute to module
-                if(!attribute.hasMapping(currentModule,issueType))
+                if (!attribute.hasMapping(currentModule, issueType))
                 {
                       currentModule.addRModuleAttribute(issueType,attribute);
                       scarabR.setConfirmMessage(l10n.get("AttributeAdded"));
@@ -521,7 +522,7 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
                                 + issueType.getIssueTypeId());
                 }
                 // Add user attribute to issue type
-                if(!attribute.hasMapping(issueType))
+                if (!attribute.hasGlobalMapping(issueType))
                 {
                    issueType.addRIssueTypeAttribute(attribute);
                    scarabR.setConfirmMessage(l10n.get("AttributeAdded"));
