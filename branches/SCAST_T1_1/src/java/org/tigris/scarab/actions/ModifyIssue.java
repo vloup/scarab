a2cdf92b069cc7715ec4b426a8d174ce07cb8b51
venkatesh <venkatesh@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-08-01 19:06:53 +0000

    Change log: File: src/java/org/tigris/scarab/actions/ModifyIssue.java #doAdddependency(..)-set the field message when the user tries to add an issue dependency that already exists. Alert message need not be set here since it would any way be overwritten by "saveDependencyChanges()".
    Submitted by: venkat@collab.net
    Reviewed by: kmaples@collab.net
    Issue: PCN20937
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@8426 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCAST_T1_1/src/java/org/tigris/scarab/actions/ModifyIssue.java
+++ branches/SCAST_T1_1/src/java/org/tigris/scarab/actions/ModifyIssue.java
@@ -820,7 +820,7 @@ public class ModifyIssue extends BaseModifyIssue
      *  Modifies the dependency type between the current issue
      *  And its parent or child issue.
      */
-    private boolean doAdddependency(Issue issue, IntakeTool intake, 
+    private boolean doAdddependency(Issue issue, IntakeTool intake,
                                  Group group, ScarabRequestTool scarabR,
                                  TemplateContext context,
                                  ScarabLocalizationTool l10n,
@@ -895,7 +895,7 @@ public class ModifyIssue extends BaseModifyIssue
             }
             catch (ScarabException se)
             {
-                scarabR.setAlertMessage(l10n.get(se.getMessage()));
+                childId.setMessage(l10n.get(se.getMessage()));
                 return false;
             }
             catch (Exception e)
