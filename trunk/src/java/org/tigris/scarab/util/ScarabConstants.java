36aff13285dd7dd9a89bd06948ea666caca9c3b2
dlr <dlr@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2003-09-03 22:15:01 +0000

    * src/i18n/ScarabBundle_en_US.properties   IssueLimitExceeded: New resource added to alert the user when more   than the configured number of artifacts is selected for an action.
    * src/templates/macros/GlobalMacros.vm
      #processQueryResults(): One more argument (issue count) is added to
      the macro to conditionally display the "Xxx all" options.
    
    
    * src/templates/screens/IssueList.vm
      #processQueryResults() is called regardless of the number of
      artifacts in the search result. Pass that number to the macro.
    
    
    * src/conf/Scarab.properties
      scarab.issue.max.view, scarab.issue.max.assign,
      scarab.issue.max.move, scarab.issue.max.copy: New properties added
      to configure the max issues that can be viewed/assigned/copied/moved
      simultaneously.
    
    
    * src/java/org/tigris/scarab/util/ScarabConstants.java
      ISSUE_MAX_VIEW, ISSUE_MAX_ASSIGN, ISSUE_MAX_MOVE, ISSUE_MAX_COPY:
      New constants added to hold the value of the properties mentioned
      above.
    
    
    * src/java/org/tigris/scarab/actions/Search.java
      doPerform(): Condition added to alert the user when more than the
      configured number of artifacts is selected for an action.
    
    
    Issue: PCN21658
    Submitted by: Venkat
    Reviewed by: Daniel Rall
    
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@8554 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/util/ScarabConstants.java
+++ trunk/src/java/org/tigris/scarab/util/ScarabConstants.java
@@ -247,4 +247,27 @@ public interface ScarabConstants
 
     Integer INTEGER_0 = new Integer(0);
 
+    /**
+     * The maximum number of issues for batch view of detail.
+     */
+    int ISSUE_MAX_VIEW =
+        Turbine.getConfiguration().getInt("scarab.issue.max.view", 25);
+
+    /**
+     * The maximum number of issues for batch assign.
+     */
+    int ISSUE_MAX_ASSIGN =
+        Turbine.getConfiguration().getInt("scarab.issue.max.assign", 25);
+
+    /**
+     * The maximum number of issues for batch copy.
+     */
+    int ISSUE_MAX_COPY =
+        Turbine.getConfiguration().getInt("scarab.issue.max.copy", 250);
+
+    /**
+     * The maximum number of issues for batch move.
+     */
+    int ISSUE_MAX_MOVE =
+        Turbine.getConfiguration().getInt("scarab.issue.max.move", 250);
 }    
