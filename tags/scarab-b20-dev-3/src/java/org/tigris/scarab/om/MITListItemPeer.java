b78e8124d4969d29a03a88aef2604cc6ffecd5b3
jorgeuriarte <jorgeuriarte@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2005-02-18 21:06:59 +0000

    --non-interactive
    git-svn-id: http://scarab.tigris.org/svn/scarab@9418 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ tags/scarab-b20-dev-3/src/java/org/tigris/scarab/om/MITListItemPeer.java
@@ -0,0 +1,12 @@
+package org.tigris.scarab.om;
+
+
+/** 
+ *  You should add additional methods to this class to meet the
+ *  application requirements.  This class will only be generated as
+ *  long as it does not already exist in the output directory.
+ */
+public class MITListItemPeer 
+    extends org.tigris.scarab.om.BaseMITListItemPeer
+{
+}
