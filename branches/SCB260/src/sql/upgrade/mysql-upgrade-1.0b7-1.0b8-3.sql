fb1eb39660a414ef7c213a6f0d92e9644e33b10a
jorgeuriarte <jorgeuriarte@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2005-03-15 22:20:28 +0000

    Removing old branches...
    git-svn-id: http://scarab.tigris.org/svn/scarab@9468 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCB260/src/sql/upgrade/mysql-upgrade-1.0b7-1.0b8-3.sql
+++
@@ -1,9 +0,0 @@
-/*
- * This upgrade script increases the length of 
- * SCARAB_ATTACHMENT.ATTACHMENT_MIME_TYPE column.  
- * 25 is not long enough, don't know what is maximum, so go with 255.
- *
- * Created By: John McNally
- * $Id$ */
-
-alter table SCARAB_ATTACHMENT change ATTACHMENT_MIME_TYPE ATTACHMENT_MIME_TYPE varchar(255) NOT NULL; 
