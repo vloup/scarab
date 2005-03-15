fe642e0d18c02f190191eb9a1e334e72598e7e88
jorgeuriarte <jorgeuriarte@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2005-03-15 21:56:47 +0000

    Removing old branches
    git-svn-id: http://scarab.tigris.org/svn/scarab@9465 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/SCARAB_1_0_B8_TEST_BRANCH/src/sql/upgrade/mysql-upgrade-1.0b7-1.0b8-7.sql
+++
@@ -1,8 +0,0 @@
-/*
- * This upgrade script adds an ATTACHMENT_ID column to the SCARAB_ACTIVITY table.
- *
- * Created By: Jon Scott Stevens
- * $Id$
- */
-
-ALTER TABLE SCARAB_ACTIVITY ADD column ATTACHMENT_ID integer null default 0;
