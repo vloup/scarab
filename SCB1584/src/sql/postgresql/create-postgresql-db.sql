19164ac838106d8a26e09bb05463a5ff81cb1976
hair <hair@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2005-12-07 06:35:09 +0000

    oops! (forgot "branches/")
    git-svn-id: http://scarab.tigris.org/svn/scarab@9974 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- SCB1584/src/sql/postgresql/create-postgresql-db.sql
+++
@@ -1,5 +0,0 @@
-drop database @DB_NAME@;
-drop user @DATABASE_USERNAME@;
-create user @DATABASE_USERNAME@ with encrypted password '@DATABASE_PASSWORD@';
-create database @DB_NAME@ with owner @DATABASE_USERNAME@;
- 
