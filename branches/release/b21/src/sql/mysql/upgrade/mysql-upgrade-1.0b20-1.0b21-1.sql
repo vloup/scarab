eb7c86250ee29cf69a702564e231a935d7a65af9
dabbous <dabbous@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2006-06-09 20:20:51 +0000

    fixed some issues with batch file syntax for MySQL-4.1 RDB engines, mainly changed the comments syntax in the upgrade sql-scripts from java style "/* ... */"  to sql style "-- ..."
    and added some code to prevent MySQL from checking for foreign key
    constraints during table creation.
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@10158 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/release/b21/src/sql/mysql/upgrade/mysql-upgrade-1.0b20-1.0b21-1.sql
+++ branches/release/b21/src/sql/mysql/upgrade/mysql-upgrade-1.0b20-1.0b21-1.sql
@@ -1,35 +1,34 @@
-/*
- * b20-b21 Migration.
- *
- * Creates new fields to allow deleting issues
- * Creates new permissions for commenting and deleting issues
- *
- */
+--
+-- b20-b21 Migration.
+--
+-- Creates new fields to allow deleting issues
+-- Creates new permissions for commenting and deleting issues
+--
  
-/**
-  * Adds the field to describe ActivityType for every activity record.
-  */
+-- 
+--  Adds the field to describe ActivityType for every activity record.
+--  
 ALTER TABLE SCARAB_ACTIVITY ADD ACTIVITY_TYPE varchar(30) null;
 
-/**
-  * Adds the field to mark an issue as 'moved'. The 'deleted' will now be
-  * used just for the deletions.
-  */
+-- 
+--  Adds the field to mark an issue as 'moved'. The 'deleted' will now be
+--  used just for the deletions.
+--  
 ALTER TABLE SCARAB_ISSUE ADD MOVED INTEGER(1);
 
-/**
-  * Move the 'deleted' values into 'moved' field.
-  */
+-- 
+--  Move the 'deleted' values into 'moved' field.
+--  
 update SCARAB_ISSUE SET MOVED=DELETED;
 
-/**
-  * Reset the 'deleted' issue.
-  */
+-- 
+--  Reset the 'deleted' issue.
+--  
 update SCARAB_ISSUE SET DELETED=0;
 
-/**
-  * Insert new permissions (delete & comment)
-  */
+-- 
+--  Insert new permissions (delete & comment)
+--  
 INSERT INTO TURBINE_PERMISSION (PERMISSION_ID, PERMISSION_NAME) VALUES (21, 'Issue | Comment');
  
 INSERT INTO TURBINE_PERMISSION (PERMISSION_ID, PERMISSION_NAME) VALUES (20, 'Issue | Delete');
