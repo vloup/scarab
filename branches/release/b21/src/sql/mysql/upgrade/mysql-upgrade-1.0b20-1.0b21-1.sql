86f949f82a78e4822afe81c36573f70fabc9db3e
jorgeuriarte <jorgeuriarte@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2006-06-11 14:50:27 +0000

    Provides 'comment' permission granting for b21-migrated instances.
    git-svn-id: http://scarab.tigris.org/svn/scarab@10163 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/release/b21/src/sql/mysql/upgrade/mysql-upgrade-1.0b20-1.0b21-1.sql
+++ branches/release/b21/src/sql/mysql/upgrade/mysql-upgrade-1.0b20-1.0b21-1.sql
@@ -32,3 +32,14 @@ update SCARAB_ISSUE SET DELETED=0;
 INSERT INTO TURBINE_PERMISSION (PERMISSION_ID, PERMISSION_NAME) VALUES (21, 'Issue | Comment');
  
 INSERT INTO TURBINE_PERMISSION (PERMISSION_ID, PERMISSION_NAME) VALUES (20, 'Issue | Delete');
+
+--
+-- Grant the new 'comment' (21) permission to anyone previously having 'edit' (1)
+-- permission
+--
+create table scarab_b20_b21_perms
+select role_id, 21 as permission_id from turbine_role_permission where permission_id = 1;
+
+insert into turbine_role_permission select role_id, permission_id from scarab_b20_b21_perms;
+
+drop table scarab_b20_b21_perms;
