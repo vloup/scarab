75e49bc12f18cd16f863e8866e4528986e39d648
dabbous <dabbous@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2007-02-16 08:48:04 +0000

    folder moved to tags/release/SCARAB_0_21
    git-svn-id: http://scarab.tigris.org/svn/scarab@10416 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- tags/SCARAB_0_21/src/sql/postgresql/upgrade/postgresql-upgrade-1.0b13-1.0b14-11.sql
+++
@@ -1,8 +0,0 @@
-/*
- * Drops the unique constraint on the NAME column.  
- *
- * Created: Sean Jackson <sean@pnc.com.au>
- * $Id$
- */
-
-DROP INDEX scarab_global_paramete_name_key;
--- tags/SCARAB_0_21/src/sql/postgresql/upgrade/postgresql-upgrade-1.0b13-1.0b14-14.sql
+++
@@ -1,9 +0,0 @@
-/*
- * ACCEPT_LANGUAGE was not an acceptable name so we are
- * changing it to just LANGUAGE to be more generic.
- * 
- * Created: Sean Jackson <sean@pnc.com.au>
- * $Id$
- */
-
-ALTER TABLE SCARAB_USER_PREFERENCE RENAME ACCEPT_LANGUAGE TO LANGUAGE;
