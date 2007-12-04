63ecc45e569988b98d8d8f19950163c4899add40
ronvoe122 <ronvoe122@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2007-12-04 16:04:19 +0000

    SCB2413: one file was missing in the last commit.
    git-svn-id: http://scarab.tigris.org/svn/scarab@10487 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ trunk/src/java/org/tigris/scarab/util/word/TextQuery.java
@@ -0,0 +1,70 @@
+package org.tigris.scarab.util.word;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.tigris.scarab.om.AttributeValue;
+
+public class TextQuery
+{
+
+    private List attrClauses = new ArrayList();
+    private List commentClauses = new ArrayList();
+    private boolean and;
+
+    public String toString()
+    {
+        String query = "";
+        
+        String connection;
+        if(and) 
+            connection = "+";
+        else 
+            connection = "";
+
+        for(Iterator i = commentClauses.iterator();i.hasNext();)
+        {
+            String clause = (String)i.next();
+            query = query + connection + LuceneSearchIndex.COMMENT + ":(" + clause + ")";
+        }
+        for(Iterator i = attrClauses.iterator();i.hasNext();)
+        {
+            AttributeValue clause = (AttributeValue)i.next();
+            query = query + connection + LuceneSearchIndex.ATTRIBUTE_ID + clause.getAttributeId().toString() + ":(" + clause.getValue() + ")";                
+        }
+        return query;
+    }
+
+    public void addAttrClause(AttributeValue clause)
+    {
+        attrClauses.add(clause);
+    }
+
+    public void addAttrClauses(List clauses)
+    {
+            attrClauses.addAll(clauses);        
+    }
+
+    public void addCommentClause(String clause)
+    {
+        if(clause!=null && !clause.equals(""))
+            commentClauses .add(clause); 
+    }
+
+    public boolean isEmpty()
+    {
+        return attrClauses.size()==0 || commentClauses.size()==0;
+    }
+
+    public void setAnd(boolean and)
+    {
+        this.and = and;
+        
+    }
+
+    public boolean isAnd()
+    {
+        return and;
+    }
+}
