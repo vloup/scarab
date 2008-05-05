d6237218e0265a38ccedbcf9dac66f0c90124eb0
hair <hair@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2008-05-05 14:35:19 +0000

    svn merge -r10490:10489 http://subversion.tigris.org/svn/scarab/trunk
    git-svn-id: http://scarab.tigris.org/svn/scarab@10555 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/java/org/tigris/scarab/om/Issue.java
+++ trunk/src/java/org/tigris/scarab/om/Issue.java
@@ -2082,11 +2082,6 @@ public class Issue
                 }
             }
         }
-        if(  getActivitySetRelatedByLastTransId()==null
-           ||getActivitySetRelatedByCreatedTransId()==null)
-        {
-            throw new RuntimeException("Created or Last ActivitySet must not be null.");
-        }
         super.save(dbCon);
     }
 
@@ -2379,12 +2374,12 @@ public class Issue
         {
             newIssue = this;
             newIssue.setIssueType(newIssueType);
-            newIssue.save();
         }
         else
         {
             newIssue = newModule.getNewIssue(newIssueType);
         }
+        newIssue.save();
 
         if (newIssue != this)
         {
@@ -4008,13 +4003,5 @@ public class Issue
     public String getIssueNewId() throws TorqueException
     {
         return ActivityPeer.getNewIssueUniqueId(this);
-    }
-
-    public void setCreatedTransId(Long createdTransId) 
-        throws TorqueException
-    {
-        super.setCreatedTransId(createdTransId);
-        if(super.getLastTransId()==null)
-            super.setLastTransId(createdTransId);
     }
 }
