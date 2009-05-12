28db03edf8f5863ac86efefa7d2f7c8d1646e3da
dabbous <dabbous@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2009-05-12 16:51:30 +0000

    SCB2834:
    - Added a preliminary solution.
    Basically a new attribute field named "style" can be filled with css-information. IN order to provide more flexibility, i have not
    tied the field to 'style' or 'class' respcetively. But i allow as input:
    
    style='color:red;'
    class='status'
    
    or combinations. Please do NOT use double quotes!!!
    
    - Added a new file skins/custom.css. This file should be empty (currently it is not!) and it should be generated from the builder. So this file will be removed again when this feature is fully implemented.
    
    - Added l10n entries for the new style field:
      DisplayStyle and
      DisplayStyleExplained
      
    - updated the upgrade-0.23-attribute.sql for the database update
    - updated scarab schema and intake for the new field
    - updated the templates
    
    git-svn-id: http://scarab.tigris.org/svn/scarab@10719 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/src/sql/upgrade/upgrade-0.23-attribute.sql
+++ trunk/src/sql/upgrade/upgrade-0.23-attribute.sql
@@ -1,10 +1,15 @@
 /*
- * Added Attribute Format Field size and Hinted field (release 0.23) 
+ * Added more fields (release 0.23)
+ * Style     : used as style and or class markup for display
+ * Format    : regular expression which the input must conform too
+ * Field size: size of displayfield (not field length!)
+ * Hint      : example input as viusal hint for user entries
  *
  * Creates the fields that support the enhanced formatting capabilities
  * of user defined Attributes
  *
  */
+ALTER TABLE SCARAB_ATTRIBUTE ADD ( STYLE VARCHAR(255) );
 ALTER TABLE SCARAB_ATTRIBUTE ADD ( FORMAT VARCHAR(255) );
 ALTER TABLE SCARAB_ATTRIBUTE ADD ( HINT VARCHAR(255) );
 ALTER TABLE SCARAB_ATTRIBUTE ADD ( FIELDSIZE INTEGER );
