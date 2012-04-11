4d293d659d6736a4f780f9e15a81a2ad7fb16cc4
mcoss <mcoss@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2012-04-11 12:58:26 +0000

    ADD - Alternative build script for maven goal "site", see SCOF106 accordingly.
    git-svn-id: http://scarab.tigris.org/svn/scarab@11086 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ branches/release/0.22/maven-site.bat
@@ -0,0 +1,10 @@
+@echo off
+REM This is an alterntive build script for maven goal "site"
+REM see SCOF106 accordingly
+
+call maven clean
+
+call maven changes:report
+
+call maven site -Dmaven.test.skip=true -Dmaven.velocity.version=1.5 -Dtorque.doc.dir=target\docs\databases
+
