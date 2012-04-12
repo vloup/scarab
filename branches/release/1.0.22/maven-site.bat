870ccb2b415f740f76e29e56be5e8ccab17abf09
jhoech <jhoech@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2012-04-12 07:41:46 +0000

    FIX - Goal changes:report is attained during pregoal of site.
    git-svn-id: http://scarab.tigris.org/svn/scarab@11090 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- branches/release/1.0.22/maven-site.bat
+++ branches/release/1.0.22/maven-site.bat
@@ -4,7 +4,5 @@ REM see SCOF106 accordingly
 
 call maven clean
 
-call maven changes:report
-
 call maven site -Dmaven.test.skip=true -Dmaven.velocity.version=1.5 -Dtorque.doc.dir=target\docs\databases
 
