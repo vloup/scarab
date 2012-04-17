a8213d249118c4a4bc641f96f9538419ff98faa6
mcoss <mcoss@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2012-04-17 08:38:37 +0000

    ADD - Batch scripts for currently supported build goals.
    git-svn-id: http://scarab.tigris.org/svn/scarab@11163 3c7165bb-d2ef-0310-b724-bafd9a86081f

---
+++ branches/20120417_mavenPort/build-jira.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM *******************************************************
+REM Build file for importing Scarab's jira templates.
+REM *******************************************************
+
+mvn initialize scarab:import-jira-templates
