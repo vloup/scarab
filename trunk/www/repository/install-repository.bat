05f5a3c8d9ffb6f31d00cb87c05fc388d2cc2e1c
jhoech <jhoech@3c7165bb-d2ef-0310-b724-bafd9a86081f>
2012-07-19 13:17:34 +0000

    Obsolete windows batch build removed.
    git-svn-id: http://scarab.tigris.org/svn/scarab@11293 3c7165bb-d2ef-0310-b724-bafd9a86081f

--- trunk/www/repository/install-repository.bat
+++
@@ -1,6 +0,0 @@
-call mvn install:install-file -DgroupId=fulcrum -DartifactId=fulcrum-parser -Dversion=1.0.2-dev-r223202-patched -Dfile=fulcrum/jars/fulcrum-parser-1.0.2-dev-r223202-patched.jar -Dpackaging=jar
-
-call mvn install:install-file -DgroupId=turbine -DartifactId=turbine -Dversion=20041117.142852 -Dfile=turbine/jars/turbine-20041117.142852.jar -Dpackaging=jar
-
-REM do the same for jndi-1.2.1.jar
-
